90d749d1936ae9846b8ecd7f94e52d4c9d61258e
Alberto Corrales <alberto.corrales@fenergo.com>
2020-10-05 14:21:33 +0200

    Add example for issue

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using EventStore.ClientAPI;
+using System;
+using System.Text;
+
+namespace EventStoreConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conn = EventStoreConnection.Create("ConnectTo=tcp://admin:changeit@127.0.0.1:1113", "test-conn");
+            conn.ConnectAsync().Wait();
+
+            var streamName = "TestItem+3e93748f-d625-46c8-9574-588e81b03698";
+            var eventType = "event-type";
+            var data = "{ \"a\":\"2\"}";
+            var metadata = "{}";
+            var eventPayload = new EventData(Guid.NewGuid(), eventType, true, Encoding.UTF8.GetBytes(data), Encoding.UTF8.GetBytes(metadata));
+            conn.AppendToStreamAsync(streamName, ExpectedVersion.Any, eventPayload).Wait();
+        }
+    }
+}
